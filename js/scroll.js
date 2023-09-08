39b478ebcf087ba87fcf5831d0b3258338832c70
aleksey-prokopchuk <97093639+aleksey-prokopchuk@users.noreply.github.com>
2023-09-08 06:57:10 +0300

    завершив десктопну версію

--- js/scroll.js
+++ js/scroll.js
@@ -1,24 +1,39 @@
-$(document).ready(function(){
-  // Добавить плавную прокрутку до всех ссылок
-  $("a").on('click', function(event) {
+// $(document).ready(function(){
+//   // Добавить плавную прокрутку до всех ссылок
+//   $("a").on('click', function(event) {
 
-    // Убедись в этом что .hash имеет значение перед переопределением поведения по умолчанию
-    if (this.hash !== "") {
-      // Запретить поведение щелчка якоря по умолчанию
-      event.preventDefault();
+//     // Убедись в этом что .hash имеет значение перед переопределением поведения по умолчанию
+//     if (this.hash !== "") {
+//       // Запретить поведение щелчка якоря по умолчанию
+//       event.preventDefault();
 
-      // Хранить хэш
-      const hash = this.hash;
+//       // Хранить хэш
+//       const hash = this.hash;
 
-      // Использование метода animate() jQuery для добавления плавной прокрутки страницы
-      // Необязательное число (800) указывает количество миллисекунд, необходимых для прокрутки до указанной области
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
+//       // Использование метода animate() jQuery для добавления плавной прокрутки страницы
+//       // Необязательное число (800) указывает количество миллисекунд, необходимых для прокрутки до указанной области
+//       $('html, body').animate({
+//         scrollTop: $(hash).offset().top
+//       }, 800, function(){
 
-        // Добавить хэш (#) для URL-адреса после завершения прокрутки (поведение щелчка по умолчанию)
-        window.location.hash = hash;
-      });
-    } // Конец, если
+//         // Добавить хэш (#) для URL-адреса после завершения прокрутки (поведение щелчка по умолчанию)
+//         window.location.hash = hash;
+//       });
+//     } // Конец, если
+//   });
+// });
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const blockID = anchor.getAttribute("href").substr(1);
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
   });
-});
+}
