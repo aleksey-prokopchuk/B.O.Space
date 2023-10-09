02743337321c1368489d0c32aaea02be4c7694cf
aleksey-prokopchuk <97093639+aleksey-prokopchuk@users.noreply.github.com>
2023-10-09 12:08:34 +0300

    Кінцеві правки
    Видалив зайві скрипти, зробив мобільне меню

---
+++ js/mobile-menu.js
@@ -0,0 +1,29 @@
+(() => {
+    const menuBtnRef = document.querySelector('[data-menu-button]');
+    const mobileMenuRef = document.querySelector('[data-menu]');
+    const body = document.querySelector('body');
+  
+    
+    const menuServices = document.querySelector('[data-menu-about]');
+    const menuPrise = document.querySelector('[data-menu-price]');
+    const menuOrder = document.querySelector('[data-menu-pupils]');
+    const menuAbout = document.querySelector('[data-menu-rules]');
+    const menuContacts = document.querySelector('[data-menu-contacts]');
+  
+    const mobileMenu = (() => {
+        const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+        menuBtnRef.classList.toggle('is-active');
+        menuBtnRef.setAttribute('aria-expanded', !expanded);
+        mobileMenuRef.classList.toggle('is-open');
+        body.classList.toggle('no-scroll');
+    })
+
+    menuBtnRef.addEventListener('click', (mobileMenu));
+    menuServices.addEventListener('click', (mobileMenu));
+    menuPrise.addEventListener('click', (mobileMenu));
+    menuOrder.addEventListener('click', (mobileMenu));
+    menuAbout.addEventListener('click', (mobileMenu));
+    menuContacts.addEventListener('click', (mobileMenu));    
+})();
+
+
